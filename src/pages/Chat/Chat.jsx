eba0ba2bb41f3195c5682062a67d6780f54e3d7d
Binnukrishna <binnukrishna5233@gmail.com>
2024-08-04 23:49:32 +0530

    ai and user messages separated

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,90 +1,135 @@
-import { useState,useRef,useEffect } from "react";
-import Nav from "../../components/Nav/Nav"
-import "./Chat.scss"
+import { useState, useRef, useEffect } from "react";
+import Nav from "../../components/Nav/Nav";
+import "./Chat.scss";
 import { FaArrowRight } from "react-icons/fa6";
-
+import axios from "axios";
 
 const Chat = () => {
+  const [message, setMessage] = useState([]);
+  const [chat, setChat] = useState("");
+  const url = "http://127.0.0.1:8000/";
 
-  const [message,setMessage] = useState([])
-  const [chat,setChat] = useState("")
+  const aiCall = async (chat) => {
+    try {
+      const response = await axios.post(`${url}api/chatbot/`, {
+        user_message: chat,
+      });
+      console.log("res: ", response);
 
-  const sendMessage = ()=>{
-
-    if(chat.trim() !== ""){
-      setMessage([...message,chat])
-      setChat("")
-    } 
-
-  }
-
-  const keyPressHandler = (e)=>{
-    if(e.key === "Enter"){
-      e.preventDefault()
-      sendMessage()
-      console.log("enter");
-      
+      setMessage((prevState) => [
+        ...prevState,
+        {
+          ai: response.data.bot_response,
+        },
+      ]);
+    } catch (error) {
+      console.error("Error sending msg. to AI:", error);
     }
-  }
+  };
+
+  const sendMessage = () => {
+    aiCall(chat);
+
+    if (chat.trim() !== "") {
+      setMessage((prevState) => [
+        ...prevState,
+        {
+          user: chat,
+        },
+      ]);
+
+      setChat("");
+    }
+  };
+
+  console.log(message);
+
+  const keyPressHandler = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sendMessage();
+      console.log("enter");
+    }
+  };
 
   const chatHistRef = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (chatHistRef.current) {
       chatHistRef.current.scrollTop = chatHistRef.current.scrollHeight;
+      console.log("scrolling");
     }
-  }, [message])
+  }, [message]);
 
   return (
     <>
-        <Nav/>
-        <div className="chat_container">
-          <div className="title">
-            <h3>
-              Chat with your&nbsp;
-              <span>
-                health Companion!
-              </span> 
-            </h3>
-          </div>
+      <Nav />
+      <div className="chat_container">
+        <div className="title">
+          <h3>
+            Chat with your&nbsp;
+            <span>health Companion!</span>
+          </h3>
+        </div>
 
+        <div className="chat_sec">
+          <div className="chat_hist" ref={chatHistRef}>
+            {message.map((item, index) =>
+              item.ai ? (
+                <div className="chat_AI" key={index}>
+                  <div className="bubble">
+                    <p>{item.ai}</p>
+                  </div>
+                </div>
+              ) : (
+                <div className="chat_user" key={index}>
+                  <div className="bubble">
+                    <p>{item.user}</p>
+                  </div>
+                </div>
+              )
+            )}
 
-          <div className="chat_sec">
-            <div className="chat_hist" ref={chatHistRef}>
-              <div className="chat_AI">
+            {/* <div className="chat_AI">
+              <div className="bubble">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, ab?
+                </p>
+              </div>
+            </div>
+
+            {message.map((item, index) => (
+              <div className="chat_user" key={index}>
                 <div className="bubble">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab?
-                  </p>
+                  <p>{item.user}</p>
                 </div>
               </div>
+            ))} */}
+          </div>
 
-              {
-                message.map((item,index)=>(
-                  <div className="chat_user" key={index}>
-                    <div className="bubble">
-                      <p>
-                        { item }
-                      </p>
-                    </div>
-                  </div>
-                ))
-              } 
-
-            </div>
-
-            <div className="input_container">
-              <input type="text" placeholder="ask anything!" value={ chat } onChange={ (e)=>{ setChat(e.target.value) }} onKeyDown={ keyPressHandler }/> 
-              <button id="send" onClick={ sendMessage} >
-                <FaArrowRight style={{
-                  color:"white"
-                }}/>
-              </button>
-            </div>
+          <div className="input_container">
+            <input
+              type="text"
+              placeholder="ask anything!"
+              value={chat}
+              onChange={(e) => {
+                setChat(e.target.value);
+              }}
+              onKeyDown={keyPressHandler}
+            />
+            <button id="send" onClick={sendMessage}>
+              <FaArrowRight
+                style={{
+                  color: "white",
+                }}
+              />
+            </button>
           </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
